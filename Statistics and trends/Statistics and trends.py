e946379241e335551ec18d29775189a37c4bd25f
Lalith Kumar Redrouthu <lalithkumar.r420@gmail.com>
2022-12-08 07:02:20 +0000

    added line plot function

--- Statistics and trends/Statistics and trends.py
+++ Statistics and trends/Statistics and trends.py
@@ -13,21 +13,23 @@ Topics choosen for comparision are
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-class dataFormation(object):
+class dataFormation:
     def __init__(self, fileName):
         dataBank = pd.read_csv(fileName)
         dataBank = dataBank.dropna(axis='columns', how='all')
-        dataBank = dataBank.replace(np.NaN,0)
-        dataBank = dataBank.drop(['Country Code', 'Indicator Name', 'Indicator Code'], axis=1) 
+        # dataBank = dataBank.replace(np.NaN,0)
+        toDrop = ['Country Code', 'Indicator Name', 'Indicator Code']
+        dataBank = dataBank.drop(toDrop, axis=1) 
         
         self.dataCountries = dataBank
         
         dataBank_t = pd.DataFrame.transpose(dataBank)
         dataBank_t = dataBank_t.rename(index={"Country Name":"Year"})
+        dataBank_t = dataBank_t.reset_index()
         dataBank_t.columns = dataBank_t.iloc[0]
         dataBank_t = dataBank_t.iloc[1:]
-        
         
         self.dataYears =  dataBank_t
         
@@ -37,9 +39,22 @@ class dataFormation(object):
     def data_years(self):
         return self.dataYears
     
+
+def linePlot(data, countries):
+    # print(data)
+    data.plot('Year', countries, label= countries, kind = "line", fontsize = 22)
+    # data.plot(xlabel='X Label', ylabel='Y Label', title='Plot Title')
+    
+    return
+    
     
 data_co2 = dataFormation("CO2 emissions (kt).csv")
 data_energy = dataFormation("Energy use (kg of oil equivalent per capita).csv")
 
+county_list = ['Bolivia', 'Denmark', 'Finland', 'Georgia', 'Switzerland']
+
 # print(data_co2.data_countries())
-print(data_co2.data_years())
+# print(data_co2.data_years())
+
+linePlot(data_co2.data_years(),county_list)
+linePlot(data_energy.data_years(),county_list)
