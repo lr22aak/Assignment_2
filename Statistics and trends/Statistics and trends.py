e4c910b3b23a8bad3c763b082e4ce0265edb5f52
Lalith Kumar Redrouthu <lalithkumar.r420@gmail.com>
2022-12-14 14:29:08 +0000

    added line plots and pie plots code

--- Statistics and trends/Statistics and trends.py
+++ Statistics and trends/Statistics and trends.py
@@ -7,19 +7,21 @@ Assignment 2: Statistics and trends
 Topics choosen for comparision are 
 - CO2 emissions (kt)
 - Energy use (kg of oil equivalent per capita)
+- Forest area (% of land area)
+- Mortality rate, neonatal (per 1,000 live births)
+- Total greenhouse gas emissions (kt of CO2 equivalent)
 
 @author: lalit
 """
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
 
 class dataFormation:
     def __init__(self, fileName):
         dataBank = pd.read_csv(fileName)
         dataBank = dataBank.dropna(axis='columns', how='all')
-        # dataBank = dataBank.replace(np.NaN,0)
         toDrop = ['Country Code', 'Indicator Name', 'Indicator Code']
         dataBank = dataBank.drop(toDrop, axis=1) 
         
@@ -40,21 +42,95 @@ class dataFormation:
         return self.dataYears
     
 
-def linePlot(data, countries):
+def linePlot(data, countries, Title, fontSize):
     # print(data)
-    data.plot('Year', countries, label= countries, kind = "line", fontsize = 22)
-    # data.plot(xlabel='X Label', ylabel='Y Label', title='Plot Title')
+    imageName = Title+" line_Plot.png"
+    data.plot('Year', countries,
+              label= countries, 
+              kind = "line", fontsize = fontSize, title = Title)
+    plt.grid()
+    plt.rc('legend', fontsize=fontSize)
+    plt.title(Title, size = 35)
+    plt.xlabel('Year',size = fontSize)
+    plt.savefig(imageName)
+    plt.show()
     
     return
     
+
+def barPlot(data, countries, years, fontSize, Title):
+    imageName = Title+" bar_plot.png"
+    # extract countries of interest
+    df_plot = data.loc[data['Country Name'].isin(countries)]
+    
+    # df_plot.plot.bar('Country Name', years)
+    df_plot.plot('Country Name', years,
+              label= countries, 
+              kind = "bar", fontsize = fontSize, title = Title)
+    plt.title(Title, size = 35)
+    plt.ticklabel_format()
+    plt.xlabel('Country Names',size = fontSize)
+    plt.legend(years)
+    plt.savefig(imageName)
+    plt.show()
+    
+    return
+
+def piePlot(data, countries, Title, year):
+    imageName = Title+" pie_plot.png"
+    # extract countries of interest
+    df_plotPie = data.loc[data['Country Name'].isin(countries)]
+    df_plotPie = df_plotPie[["Country Name", year]]
+    print(df_plotPie)
+    # df_1990["1990"] = pd.to_numeric(df_1990["1990"])
+    
+    
+    
+    df_plotPie[year] = pd.to_numeric(df_plotPie[year])
+    
+    plt.pie(df_plotPie[year], labels = countries)
+    plt.rc('legend', fontsize=25)
+    plt.title(Title+year, size = 35)
+    plt.savefig(imageName)
+    plt.show()
+    return
     
 data_co2 = dataFormation("CO2 emissions (kt).csv")
 data_energy = dataFormation("Energy use (kg of oil equivalent per capita).csv")
+data_forest = dataFormation("Forest area (% of land area).csv")
+data_mortality = dataFormation("Mortality rate.csv")
+data_greenhouse_gas = dataFormation("greenhouse gas emissions.csv")
 
-county_list = ['Bolivia', 'Denmark', 'Finland', 'Georgia', 'Switzerland']
+county_list = ['Bolivia', 'Denmark', 'Finland', 'Greece', 'Switzerland']
+years_list = ['1995', '2000', '2005', '2010', '2015']
 
-# print(data_co2.data_countries())
-# print(data_co2.data_years())
 
-linePlot(data_co2.data_years(),county_list)
-linePlot(data_energy.data_years(),county_list)
+# linePlot(data_co2.data_years(),
+#           county_list, 
+#           "CO2 emissions (kt)",
+#           25)
+
+# linePlot(data_energy.data_years(), 
+#           county_list, 
+#           "Energy use (kg of oil equivalent per capita)",
+#           25)
+
+# barPlot(data_greenhouse_gas.data_countries(), 
+#         county_list, 
+#         years_list, 25,
+#         "Total greenhouse gas emissions (kt of CO2 equivalent)")
+
+# barPlot(data_mortality.data_countries(), 
+#         county_list, 
+#         years_list, 25,
+#         "Mortality rate, neonatal (per 1,000 live births)")
+
+piePlot(data_co2.data_countries(), 
+        county_list,
+        "CO2 emissions (kt)", 
+        "2005")
+
+piePlot(data_co2.data_countries(), 
+        county_list,
+        "CO2 emissions (kt)", 
+        "2014")
